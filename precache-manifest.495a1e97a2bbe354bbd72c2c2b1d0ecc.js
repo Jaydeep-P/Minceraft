7f17cea1919ccebb7cbb08fb58f06597aef18a32
Jaydeep Patel <mcpe071202@gmail.com>
2021-12-27 00:13:47 +0530

    'pages'

---
+++ precache-manifest.495a1e97a2bbe354bbd72c2c2b1d0ecc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1db9549a37b20a43fc6ade533f0cdb",
+    "url": "/Minceraft/index.html"
+  },
+  {
+    "revision": "6f80423bbbe9a75839fc",
+    "url": "/Minceraft/static/css/main.f37ab771.chunk.css"
+  },
+  {
+    "revision": "d1e7e9743af8670dab1d",
+    "url": "/Minceraft/static/js/2.50f3e916.chunk.js"
+  },
+  {
+    "revision": "b59861b9ca6e7b8651484dd212de5308",
+    "url": "/Minceraft/static/js/2.50f3e916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f80423bbbe9a75839fc",
+    "url": "/Minceraft/static/js/main.60c1c2f0.chunk.js"
+  },
+  {
+    "revision": "740b49d02e6e197d2aaf",
+    "url": "/Minceraft/static/js/runtime-main.d1674327.js"
+  },
+  {
+    "revision": "370d4eafa9a5419af84ecc5bd785d9e8",
+    "url": "/Minceraft/static/media/Minecraftia.370d4eaf.woff"
+  },
+  {
+    "revision": "e771507db31f686d69fa6b64ac2e61d9",
+    "url": "/Minceraft/static/media/MojangLogo.e771507d.svg"
+  },
+  {
+    "revision": "aa22cf09df1ba380b0c6ed5704be6b0e",
+    "url": "/Minceraft/static/media/back-ground0.aa22cf09.jpeg"
+  },
+  {
+    "revision": "b7e4203a856eb68d965579ba854e5c02",
+    "url": "/Minceraft/static/media/back-ground1.b7e4203a.jpeg"
+  },
+  {
+    "revision": "15fb70a1839a5b95f90a389d3ff70cea",
+    "url": "/Minceraft/static/media/back-ground2.15fb70a1.jpeg"
+  },
+  {
+    "revision": "186b3d3fa2f9b3a99b24d7f5272019c4",
+    "url": "/Minceraft/static/media/back-ground3.186b3d3f.jpeg"
+  },
+  {
+    "revision": "f52b5604834b4e49172e07b85ffdc2cf",
+    "url": "/Minceraft/static/media/button.f52b5604.png"
+  },
+  {
+    "revision": "efc3a0f6e5cee79e2dcbffe535b4673a",
+    "url": "/Minceraft/static/media/minceraftLogo.efc3a0f6.png"
+  }
+]);
